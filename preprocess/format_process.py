3ab72eacd6a86a79f75458cb92cc2e584ac94793
FoVNull <34230654+FoVNull@users.noreply.github.com>
2020-11-07 15:16:20 +0800

    init

--- preprocess/format_process.py
+++ preprocess/format_process.py
@@ -11,8 +11,8 @@ def word2sentence(path, output):
             seq.append(w+"/"+pos)
 
     with open(output, 'w', encoding='utf-8') as f:
-        for seq in texts:
-            f.write(" ".join(seq)+"\n")
+        for i, seq in enumerate(texts):
+            f.write(str(i)+"\t"+" ".join(seq)+"\n")
 
 
 word2sentence("../corpus/Ind_train-1.txt", "../corpus/Ind_train.txt")
